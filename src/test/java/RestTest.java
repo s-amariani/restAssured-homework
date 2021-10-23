a025d2d98018e8b37ca78e648616009ec3b409c4
Sergo Amariani <sergoamariani@gmail.com>
2021-10-23 04:25:10 +0400

    restAssured-basics2

--- src/test/java/RestTest.java
+++ src/test/java/RestTest.java
@@ -1,8 +1,18 @@
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -22,14 +32,14 @@ public class RestTest {
 
         JsonPath jsonPath = response.jsonPath();
 
-        String circuitIdIndex0 = jsonPath.get("MRData.CircuitTable.Circuits[1].circuitId");
+        String circuitIdIndex1 = jsonPath.get("MRData.CircuitTable.Circuits[1].circuitId");
         String circuitIdIndex5 = jsonPath.get("MRData.CircuitTable.Circuits[5].circuitId");
         //get country value to validate country in "validateCountry()" test
-        String countryIndex0 = jsonPath.get("MRData.CircuitTable.Circuits[1].Location.country");
+        String countryIndex1 = jsonPath.get("MRData.CircuitTable.Circuits[1].Location.country");
         String countryIndex5 = jsonPath.get("MRData.CircuitTable.Circuits[5].Location.country");
 
         return new Object[][]{
-                new Object[] {circuitIdIndex0, countryIndex0},
+                new Object[] {circuitIdIndex1, countryIndex1},
                 new Object[] {circuitIdIndex5,countryIndex5}
         };
     }
@@ -45,5 +55,85 @@ public class RestTest {
                 .log().body()
                 .statusCode(200)
                 .body("MRData.CircuitTable.Circuit.Location.Country",equalTo(country));
+    }
+
+    @Test
+    public void validateLastRecordsName(){
+        baseURI = "https://chercher.tech/sample/api/product/read";
+        given()
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                //Validate last record's name value
+                .body("records[-1].name",equalTo("CreateRecord"));
+
+    }
+
+    @Test()
+    public void validateAllRecordsCreatedTime() throws ParseException {
+        LocalDateTime dateTime = LocalDateTime.now(); // Gets the current date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String strCurrentDateTime = dateTime.format(formatter);
+        Date currentDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse(strCurrentDateTime);
+
+
+        System.out.println("current date time :  " + strCurrentDateTime);
+
+
+        baseURI = "https://chercher.tech/sample/api/product/read";
+        Response response = given()
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        JsonPath path = response.jsonPath();
+        List<String> lstcreatedTime = path.getList("records.created");
+
+        List<Date> lstConvertedCreatedTime = new ArrayList<>();
+        for (int i = 0; i < lstcreatedTime.size(); i++) {
+                lstConvertedCreatedTime.add(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                        .parse(lstcreatedTime.get(i)));
+        }
+
+        //Validate that all records 'created' time is less than current time
+        for (int i = 0; i < lstConvertedCreatedTime.size(); i++) {
+            if (lstConvertedCreatedTime.get(i).compareTo(currentDate) > 0) {
+                System.out.println("[" + i + "]Created time is after current time");
+                Assert.fail();
+            } else if (lstConvertedCreatedTime.get(i).compareTo(currentDate) < 0) {
+                System.out.println("[" + i + "]Created time is before current time");
+                Assert.assertTrue(true);
+            } else if (lstConvertedCreatedTime.get(i).compareTo(currentDate) == 0) {
+                System.out.println("[" + i + "]Created time is equal to current time");
+                Assert.fail();
+            } else {
+                System.out.println("[" + i + "]Something weird happened...");
+                Assert.fail();
+            }
+        }
+    }
+
+    @Test
+    public void postNewUser(){
+        //Post new user with any 2 parameters
+        String requestBody = "{\n" +
+                "    \"name\": \"{{TestAutomation}}\",\n" +
+                "    \"job\": \"{{Test Automation Engineer}}\"\n" +
+                "}";
+
+
+
+        baseURI = "https://reqres.in/api/users";
+        given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .post()
+                .then()
+                .log().ifStatusCodeIsEqualTo(201);//Log response data if status code is equals to 201
     }
 }
